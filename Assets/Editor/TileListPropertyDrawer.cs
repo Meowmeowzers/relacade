48c4fc4d943a83e079f747da6def8f3d929a81f5
Meowmeowzers <95127319+Meowmeowzers@users.noreply.github.com>
2023-05-14 02:05:50 +0800

    Tile Setup Custom Window
    - Added custom editor window for tile setup
    - Left over files
    - Checkpoint commit (Forgot about committing)

---
+++ Assets/Editor/TileListPropertyDrawer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace HelloWorld
+{
+    /*
+    [CustomPropertyDrawer(typeof(TileInput))]
+    public class TileListPropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            base.OnGUI(position, property, label);
+            //EditorGUI.LabelField(position, label);
+        }
+    }
+    */
+}
